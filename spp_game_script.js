ec68839606a83cdd7530ca938335b5c623fe7229
Marcelo Silveira Hayden <mshayden.1998@gmail.com>
2021-09-13 18:47:00 -0300

    .

--- spp_game_script.js
+++ spp_game_script.js
@@ -4,9 +4,10 @@ canvas.width = 1280;
 canvas.height = 720;
 const ctx = canvas.getContext('2d', { alpha: false });
 
-let xTimes = 0;
-const xTimeValue = 150;
-let message = '';
+let upPressed = false;
+let downPressed = false;
+const txtX = 30;
+const txtY = 30;
 // Conviniance variables
 const canvasWidHalf = canvas.width / 2;
 const canvasHeiHalf = canvas.height / 2;
@@ -14,22 +15,21 @@ const canvasHeiHalf = canvas.height / 2;
 const ballRadius = 8;
 let ballX = canvasWidHalf;
 let ballY = canvasHeiHalf;
-let xDir = 5;
-let yDir = -5;
+let xDir = 7;
+let yDir = -7;
+// Characters variables
+const characterWid = 10;
 // Player variables
 const playerX = 10;
-let playery = canvasHeiHalf;
+let playerY = canvasHeiHalf;
+let playerHei = 50;
 let playerScore = 0;
+let playerSpd = 5;
 // A.I. variables
 const aiX = canvas.width - 20;
 let aiY = canvasHeiHalf;
-
-function warningMessage() {
-	ctx.fillStyle = 'red';
-	ctx.font = '22px Tahoma';
-	ctx.textAlign = 'center';
-	ctx.fillText(message, canvasWidHalf, canvasHeiHalf);
-}
+let aiHei = 50;
+let aiScore = 0;
 
 function drawBall(x, y) {
 	ctx.beginPath();
@@ -39,24 +39,37 @@ function drawBall(x, y) {
 	ctx.closePath();
 }
 
-const txtX = 20;
-const txtY = 68;
-function drawScore() {
-	ctx.fillStyle = 'yellow';
-	ctx.font = '64px Tahoma';
-	ctx.textAlign = 'left';
-	ctx.fillText(playerScore, txtX, txtY);
-	ctx.textAlign = 'right';
-	ctx.fillText(playerScore, canvas.width - 2*txtX, txtY);
+function drawCharacters(color, posX, posY, sizeX, sizeY) {
+	ctx.fillStyle = color;
+	ctx.fillRect(posX, posY, sizeX, sizeY);
+}
+
+function drawScore(color, align, score, txtX) {
+	ctx.fillStyle = color;
+	ctx.font = '5rem Tahoma';
+	ctx.textAlign = align;
+	ctx.textBaseline = 'top';
+	ctx.fillText(score, txtX, txtY);
 }
 
 function drawFrame() {
 	// To clear the frame before draws something
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	if (xTimes > 0 && message != '') {
-		warningMessage();
-		xTimes--;
+	ctx.fillStyle = 'rgb(18, 18, 18)';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	// To move the player's character
+	if (playerY+playerHei > 0 || playerY+playerHei < canvas.height) {
+		if (upPressed) {
+			playerY -= playerSpd;
+		} else
+		if (downPressed) {
+			playerY += playerSpd;
+		}
 	}
+	// Draw Player
+	drawCharacters('#E64621', playerX, playerY, characterWid, playerHei);
+	// Draw A.I.
+	drawCharacters('#009962', aiX, aiY, characterWid, aiHei);
 
 	drawBall(ballX, ballY);
 
@@ -67,24 +80,40 @@ function drawFrame() {
 	if (ballY+yDir > canvas.height-ballRadius || ballY+yDir < ballRadius) {
 		yDir = -yDir;
 	}
-	// Corner easter egg
-	if (ballX <= (2 + ballRadius) && ballY >= canvas.height - (ballRadius+2)) {
-		xTimes = xTimeValue;
-		message = 'I SWEAR I SAW IT! AND IT WAS BEAUTIFUL!';
-	}
-	if (ballX < canvasWidHalf) {
-		xTimes = xTimeValue;
-		message = 'Left side is the player\'s side';
+	// To make the ball hit characters
+	if (ballY+yDir > playerY-playerHei || ballY+yDir < playerY-playerHei) {
+		if (ballX+xDir < playerX+characterWid) {
+			xDir = -xDir;
+		}
 	}
 
 	// To move the ball every frame
 	ballX += xDir;
 	ballY += yDir;
-	ctx.font = '14px Arial';
-	ctx.textAlign = 'left'
-	ctx.fillText('x:' + (ballX - ballRadius) + 'y: ' + (ballY - ballRadius), 10, 20)
 
-	drawScore();
+	drawScore('#E64621', 'left', playerScore, txtX);
+	drawScore('#009962', 'right', aiScore, canvas.width - txtX);
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if (e.key == 'Up' || e.key == 'ArrowUp') {
+		upPressed = true;
+	}
+	else if (e.key == 'Down' || e.key == 'ArrowDown') {
+		downPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if (e.key == 'Up' || e.key == 'ArrowUp') {
+		upPressed = false;
+	}
+	else if (e.key == 'Down' || e.key == 'ArrowDown') {
+		downPressed = false;
+	}
 }
 
 setInterval(drawFrame, 10);
